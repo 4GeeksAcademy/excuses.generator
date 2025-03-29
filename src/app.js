c703793cd9e96eb7cdfd302a3e007985f387b95e
JuanMa305 <jake_nel@hotmail.com>
2025-03-29 03:54:15 +0000

    termino de generador de excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+let quien = Math.floor(Math.random()*who.length )
+let accion = Math.floor(Math.random()*action.length)
+let que = Math.floor(Math.random()*what.length)
+let cuando = Math.floor(Math.random()*when.length)
+console.log(who[quien]+ " " +action[accion]+ " " + what[que]+ " " + when[cuando])
   console.log("Hello Rigo from the console!");
 };
